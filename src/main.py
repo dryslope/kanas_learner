cf5be6be0810cc9fdcba2acadeb8439c62bc4ec0
dryslope <thibault.gainche@gmail.com>
2025-03-27 17:16:45 +0100

    src: added ctrl-c handle

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 #!/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
 
 from class_game import Game
 
 def main():
-    the_game = Game()
-    the_game.prepare()
-    the_game.run_game()
+    try:
+        the_game = Game()
+        the_game.prepare()
+        the_game.run_game()
+    except KeyboardInterrupt:
+        os.system('clear -x')
+        print("Game was manually stopped, bye!")
 
 if __name__ == "__main__":
     main()
